179a56d3a4c2255984d0c4d91e7503eb1463cc24
EngAhmedElkady <ahelkady110@gmail.com>
2021-08-31 21:52:06 +0200

    add template and tips

---
+++ templeate.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include <cstdio>
+
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE
+typedef long long ll; 
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+#define INF 1000000000
+//memset(memo, -1, sizeof memo); // initialize DP memoization table with -1
+//memset(arr, 0, sizeof arr); // to clear array of integers
+
+void solve(){
+    int a, b;
+    while (scanf("%d %d", &a, &b) == 2)
+        printf("%d %d",&a,&b);
+
+}
+int main(){
+    // freopen("10911.txt", "r", stdin);
+
+    int t=1;
+    //scanf("%d",&t);
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
